ce910978092aca44f6e1b9b8f43625ad3e1ee8b2
Guy Messinger <gaimes410@gmail.com>
2022-11-16 15:31:55 +0200

    behatzlaha

--- workwithdc.py
+++ workwithdc.py
@@ -5,12 +5,11 @@ class workwithdc():#
         self.host = '4.227.226.102'
         self.domain = 'yourmom.dom'
         self.user = 'guym'
-        self.password = 'A123a123'
+        self.password = 'A123a123' # Need to take from pim with AIM
         self.session = winrm.Session(self.host, auth=('{}@{}'.format(self.user,self.domain),self.password), transport='ntlm')
 
     def unlockAccount(self, username):
         result = self.session.run_ps(f"C:\\Chatbot\\Scripts\\UnlockUser.ps1 {username}") # To run Powershell block
-        #print(result)
         print(result.std_out.decode())
         return result
 
